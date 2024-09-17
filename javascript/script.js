ffefcb30ed6359e21f68e314fa1d5972e6f437a4
eyad laza <eyadlaza@gmail.com>
2024-09-17 07:47:46 +0200

    main

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".under-header");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
-};
