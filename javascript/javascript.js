87611fef4c48b9f98c9ac6c35e2859b74d99692a
Eyad Laza <eyadlaza@gmail.com>
2024-12-16 18:02:15 +0100

    fixscript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,8 @@
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+function openDetails(id) {
+  document.getElementById(id).style.width = "100%";
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+// Funksjon for å lukke detaljer
+function closeDetails(id) {
+  document.getElementById(id).style.width = "0";
 }
